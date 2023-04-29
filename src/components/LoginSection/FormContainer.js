a4ad625c063c6c00605a967de4b5a3ff504038f2
helexxx <wojtek.stankiewicz1997@gmail.com>
2023-04-29 12:51:49 +0200

    small changes in CSS and other giphy icon

--- src/components/LoginSection/FormContainer.js
+++ src/components/LoginSection/FormContainer.js
@@ -6,7 +6,9 @@ import "./ContainerStyle.css";
 function FormContainer({ children }) {
   return (
     <div className='backgroundBody'>
+    <div className='backgroundHead'>
     <a className='login' href='/'>Gym king</a>
+    </div>
     <img class="mb-4 loginimage" src={giphy} alt="" width="250" height="200"></img>
     <Container id= 'FormContainer'>
       <Row className="justify-content-md-center">
