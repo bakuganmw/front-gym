99b476062eb2ca1ca03b532192081c153a7e96fc
Trung Anh Nguyen Nang <bakuganmw1@gmail.com>
2023-05-03 04:50:53 +0200

    login page checked,approved and changed

--- src/components/LoginSection/FormContainer.js
+++ src/components/LoginSection/FormContainer.js
@@ -1,24 +1,26 @@
-import React from 'react'
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import giphy from "../../images/giphy.gif";
+// import giphy from "../../images/giphy.gif";
 import "./ContainerStyle.css";
 
 function FormContainer({ children }) {
   return (
-    <div className='backgroundBody'>
-    <div className='backgroundHead'>
-    <a className='login' href='/'>Gym king</a>
+    <div className="backgroundBody">
+      <div className="backgroundHead">
+        <a className="login" href="/">
+          Gym king
+        </a>
+      </div>
+      {/* <img class="loginimage" src={giphy} alt="" width="250" height="200"></img> */}
+      <Container id="FormContainer">
+        <Row className="justify-content-md-center">
+          <Col xs={12} md={4}>
+            {children}
+          </Col>
+        </Row>
+      </Container>
     </div>
-    <img class="mb-4 loginimage" src={giphy} alt="" width="250" height="200"></img>
-    <Container id= 'FormContainer'>
-      <Row className="justify-content-md-center">
-        <Col xs={12} md={4}>
-          {children}
-        </Col>
-      </Row>
-    </Container>
-    </div>
-  )
+  );
 }
 
-export default FormContainer
+export default FormContainer;
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,39 +1,36 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { Row, Col } from 'react-bootstrap'
-import FormContainer from '../components/LoginSection/FormContainer'
-import LoginFooter from '../components/LoginSection/LoginFooter';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import FormContainer from "../components/LoginSection/FormContainer";
+import LoginFooter from "../components/LoginSection/LoginFooter";
 
 function LoginPage() {
-  
   return (
     <div>
-        <FormContainer>
-            <h1>Sign In</h1>
-            <Form>
-                <Form.Group className="mb-3 rounded" controlId='email'>
-                    <Form.Label>Email Address</Form.Label>
-                    <Form.Control
-                        type='email'
-                        placeholder='Email'
-                    >
-                    </Form.Control>
-                </Form.Group>
-                <Form.Group className="mb-3 rounded" controlId='password'>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        type='password'
-                        placeholder='Enter Password'
-                    >
-                    </Form.Control>
-                </Form.Group>
-                <Button className="rounded" type='submit' variant='primary' style={{ width: '100%', marginLeft: 0 }}>
-                    Sign In
-                </Button>
-            </Form>
-
-        </FormContainer>
+      <FormContainer>
+        <h1>Sign In</h1>
+        <Form>
+          <Form.Group className="mb-3 rounded" controlId="email">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type="email" placeholder="Email"></Form.Control>
+          </Form.Group>
+          <Form.Group className="mb-3 rounded" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter Password"
+            ></Form.Control>
+          </Form.Group>
+          <Button
+            className="rounded"
+            type="submit"
+            variant="primary"
+            style={{ width: "100%", marginLeft: 0 }}
+          >
+            Sign In
+          </Button>
+        </Form>
+      </FormContainer>
 
       <LoginFooter></LoginFooter>
     </div>
