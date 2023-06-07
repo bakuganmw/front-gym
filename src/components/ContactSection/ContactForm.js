ad195070b0d4b7da51b73bab38a2ae0b655f2e1f
236613 Nguyen Nang Trung Anh <236613@edu.p.lodz.pl>
2023-06-08 01:42:14 +0200

    save

--- src/components/ContactSection/ContactForm.js
+++ src/components/ContactSection/ContactForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
-
+import getCookie from "../../Utilities/functions";
 const FormStyle = styled.form`
   width: 100%;
 
@@ -44,21 +44,7 @@ const FormStyle = styled.form`
 
 export default function ContactForm() {
 
-  function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(";");
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === " ") {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) === 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
+
 
 const authHeader = getCookie("authHeader");
 
--- src/components/ListOfTrainers/indeks.js
+++ src/components/ListOfTrainers/indeks.js
@@ -1,23 +1,17 @@
 import React, { useState } from 'react'
 import axios from 'axios';
-import getCookie from '../../Utilities/functions';
 const ListOfTrainers = () => {
     
-      const authHeader = getCookie("authHeader");
       const [trainers,setTrainers] = useState([]);
       React.useEffect(() => {
         axios
-          .get("http://localhost:8080/trainers", {
-            headers: {
-              Authorization: authHeader,
-            },
-          })
+          .get("http://localhost:8080/trainers")
           .then((response) => {
             console.log(response);
             setTrainers(response.data);
           })
           .catch((err) => console.log(err));
-      }, [authHeader]);
+      }, []);
 
       
   return (
