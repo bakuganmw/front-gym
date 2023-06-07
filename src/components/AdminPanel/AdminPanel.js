fff0b4bd406ebe4ab7b695da537947c8dfb0b137
helexxx <wojtek.stankiewicz1997@gmail.com>
2023-06-07 20:21:02 +0200

    working change role with data

--- src/components/AdminPanel/AdminPanel.js
+++ src/components/AdminPanel/AdminPanel.js
@@ -1,134 +1,112 @@
-import React,  { useState }  from 'react'
-import { Table, Button } from 'react-bootstrap'
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { Table, Button } from 'react-bootstrap';
+import axios from 'axios';
 
 function AdminPanel() {
-
-    function getCookie(cname) {
-        let name = cname + "=";
-        let decodedCookie = decodeURIComponent(document.cookie);
-        let ca = decodedCookie.split(";");
-        for (let i = 0; i < ca.length; i++) {
-            let c = ca[i];
-            while (c.charAt(0) === " ") {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) === 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
+  function getCookie(cname) {
+    let name = cname + '=';
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
     }
-    const authHeader = getCookie("authHeader");
+    return '';
+  }
+  const authHeader = getCookie('authHeader');
 
-    const [userId, setUserId] = useState('0')
-    const [name, setName] = useState('')
-    const [lname, setlName] = useState('')
-    const [email, setEmail] = useState('')
-    const [role, setRole] = useState('')
+  const [users, setUsers] = useState([]);
 
-    React.useEffect(() => {
-                axios
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/users', {
+        headers: {
+          Authorization: authHeader,
+        },
+      })
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, [authHeader]);
 
-                    .get("http://localhost:8080/users/1", {
-                        headers: {
-                            Authorization: authHeader,
-                        },
-                    })
-                    .then((response) => {
-                        setUserId(response.data.id);
-                        setName(response.data.firstName);
-                        setEmail(response.data.email);
-                        setlName(response.data.lastName);
-                        setRole(response.data.role);
-                    })
-            .catch((err) => console.log(err));
-    }, [authHeader]);
+  const handleRoleChange = (userId, currentRole) => {
+    const newRole = currentRole === 'TRAINER' ? 'USER' : 'TRAINER';
+    axios
+      .patch(
+        `http://localhost:8080/users/${userId}`,
+        [
+          {
+            op: 'replace',
+            path: '/role',
+            value: newRole,
+          },
+        ],
+        {
+          headers: {
+            Authorization: authHeader,
+          },
+        }
+      )
+      .then((response) => {
+        if (response.status === 200) {
+          window.location.href = '/administration';
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        if(role == "TRAINER") {
-            axios.patch('http://localhost:8080/users/' + userId,
-                
-            [
-                {
-                    "op": "replace",
-                    "path": "/role",
-                    "value": "USER"
-                }
-            ],
-                {
-                    headers: {
-                        Authorization: authHeader
-                    }
-                }
-            )
-                .then(function (response) {
-                    if (response.status === 200) {
-                        window.location.href = '/administration';
-                    }
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-            } else {
-                axios.patch('http://localhost:8080/users/' + userId,
-                
-            [
-                {
-                    "op": "replace",
-                    "path": "/role",
-                    "value": "TRAINER"
-                }
-            ],
-                {
-                    headers: {
-                        Authorization: authHeader
-                    }
-                }
-            )
-                .then(function (response) {
-                    if (response.status === 200) {
-                        window.location.href = '/administration';
-                    }
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-            }
-        
-    };
-
-
-    return (
-        <div>
-            <h1>Users</h1>
-                        <Table striped bordered hover responsive className='table-sm'>
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>NAME</th>
-                                    <th>EMAIL</th>
-                                    <th>Trainer</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                    <tr key={userId}>
-                                        <td>{userId}</td>
-                                        <td>{name}</td>
-                                        <td>{email}</td>
-                                        <td>{role == "TRAINER" ? (
-                                            <Button onClick={submitHandler} type='submit' style={{ backgroundColor: 'green' }} />
-                                        ) : (
-                                            <Button onClick={submitHandler} type='submit' style={{ backgroundColor: 'red' }} />
-                                        )}</td>
-                                    </tr>                              
-                            </tbody>
-                        </Table>
-                    
-        </div>
-    )
+  return (
+    <div>
+      <h1>Users</h1>
+      <Table striped bordered hover responsive className="table-sm">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>EMAIL</th>
+            <th>Trainer</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.firstName}</td>
+              <td>{user.email}</td>
+              <td>
+                {user.role === 'TRAINER' ? (
+                  <Button
+                    onClick={() => handleRoleChange(user.id, user.role)}
+                    type="submit"
+                    style={{ backgroundColor: 'green' }}
+                  >
+                    Change to User
+                  </Button>
+                ) : (
+                  <Button
+                    onClick={() => handleRoleChange(user.id, user.role)}
+                    type="submit"
+                    style={{ backgroundColor: 'red' }}
+                  >
+                    Change to Trainer
+                  </Button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
 
-export default AdminPanel
+export default AdminPanel;
