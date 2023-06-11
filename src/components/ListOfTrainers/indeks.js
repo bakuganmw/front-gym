b182c3b0b11649f8e2b0cf60610030f09d9fca6d
helexxx <wojtek.stankiewicz1997@gmail.com>
2023-06-11 20:00:05 +0200

    trainers with users data

--- src/components/ListOfTrainers/indeks.js
+++ src/components/ListOfTrainers/indeks.js
@@ -1,36 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+
 const ListOfTrainers = () => {
   const [trainers, setTrainers] = useState([]);
-  React.useEffect(() => {
+
+  useEffect(() => {
     axios
       .get("http://localhost:8080/trainers")
       .then((response) => {
         console.log(response);
-        setTrainers(response.data);
+        const trainersData = response.data;
+        // Pobierz dane użytkowników po zakończeniu pobierania trenerów
+        fetchUsers(trainersData);
       })
       .catch((err) => console.log(err));
   }, []);
 
+  const fetchUsers = (trainersData) => {
+    axios
+      .get("http://localhost:8080/users")
+      .then((response) => {
+        console.log(response);
+        const usersData = response.data;
+        // Połącz dane trenerów z danymi użytkowników na podstawie trainerId
+        const trainersWithUsersData = trainersData.map((trainer) => {
+          const userData = usersData.find(
+            (user) => user.trainerId === trainer.id
+          );
+          return {
+            ...trainer,
+            userData: userData ? userData : null,
+          };
+        });
+        setTrainers(trainersWithUsersData);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="wrapper">
       <ul className="list-group list">
-        {trainers.map((trainer) => {
-          return (
-            <li className={"listItem "} key={trainer.id}>
-              <img
-                id="img"
-                src={require("../../images/1.png")}
-                alt="zs"
-                style={{ width: "200px", heigt: "200px" }}
-              />
-              <p className={"para description "}>
-                Trainer description:
-                {trainer.description}
+        {trainers.map((trainer) => (
+          <li className="listItem" key={trainer.id}>
+            <img
+              id="img"
+              src={require("../../images/1.png")}
+              alt="zs"
+              style={{ width: "200px", height: "200px" }}
+            />
+            <p className="para description">
+              Description: {trainer.description}
+            </p>
+            {trainer.userData && (
+              <p className="para">
+                Full Name: {trainer.userData.firstName} {trainer.userData.lastName}
               </p>
-            </li>
-          );
-        })}
+            )}
+          </li>
+        ))}
       </ul>
     </div>
   );
