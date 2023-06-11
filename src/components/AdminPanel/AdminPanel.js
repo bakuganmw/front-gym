450b88c9a3a32ed9cb30186f0a534be88565387f
236613 Nguyen Nang Trung Anh <236613@edu.p.lodz.pl>
2023-06-12 00:48:55 +0200

    get cookies final

--- src/components/AdminPanel/AdminPanel.js
+++ src/components/AdminPanel/AdminPanel.js
@@ -1,24 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Table, Button } from "react-bootstrap";
 import axios from "axios";
-import { buttonRole, buttonRoleUser, registerStyle } from "../../Utilities/functions";
+import getCookie, { buttonRole, buttonRoleUser, registerStyle } from "../../Utilities/functions";
 
 function AdminPanel() {
-  function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(";");
-    for (let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) === " ") {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) === 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
   const authHeader = getCookie("authHeader");
 
   const [users, setUsers] = useState([]);
--- src/components/TrainerSchedule/TrainerSchedule.js
+++ src/components/TrainerSchedule/TrainerSchedule.js
@@ -1,24 +1,10 @@
 import React, { useState } from "react";
 import "./TrainerSchedule.css"
 import axios from "axios";
+import getCookie from "../../Utilities/functions";
 
 const TrainerSchedule = () => {
 
-	function getCookie(cname) {
-        let name = cname + "=";
-        let decodedCookie = decodeURIComponent(document.cookie);
-        let ca = decodedCookie.split(";");
-        for (let i = 0; i < ca.length; i++) {
-            let c = ca[i];
-            while (c.charAt(0) === " ") {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) === 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
-    }
 
 	const authHeader = getCookie("authHeader");
 
